332c9158329754328fbf2a08d3defade1c272325
Bozena <test@test.com>
2021-10-09 14:55:03 +0200

    Add class Osoba

--- src/com/programowanieNarz/Main.java
+++ src/com/programowanieNarz/Main.java
@@ -3,6 +3,6 @@ package com.programowanieNarz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Osoba osoba = new Osoba("Paweł", "Makowski");
     }
 }
---
+++ src/com/programowanieNarz/Osoba.java
@@ -0,0 +1,15 @@
+package com.programowanieNarz;
+
+public class Osoba {
+    String imie;
+    String nazwisko;
+    public Osoba() {
+        imie="";
+        nazwisko="";
+    }
+
+    public Osoba(String imie, String nazwisko){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+}

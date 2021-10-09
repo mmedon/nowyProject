e6271598a7d2677b400e4c8bf968be602f34607c
Student <test@test.exe>
2021-10-09 14:55:40 +0200

    Commit Aga

--- src/com/programowanieNarz/Main.java
+++ src/com/programowanieNarz/Main.java
@@ -1,8 +1,11 @@
 package com.programowanieNarz;
-
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+	Scanner scan = new Scanner(System.in);
+    System.out.println("Podaj swoje imię: ");
+    String name = scan.nextLine();
+
+    System.out.println("Hello " + name);
     }
 }
